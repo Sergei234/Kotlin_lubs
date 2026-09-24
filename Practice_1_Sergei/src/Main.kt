fa964f24b8b0240855e2a946f7f2156a77535767
Sergei234 <125736440+Sergei234@users.noreply.github.com>
2026-09-25 00:22:36 +0600

    Add files via upload

---
+++ Practice_1_Sergei/src/Main.kt
@@ -0,0 +1,37 @@
+//task 1
+fun main(){
+    val my_f_name = "Sergei"
+    println("Hello, $my_f_name !")
+    println("Kotlin lab 01")
+
+//task 2
+    print("Enter a line: ")
+    val user_input = readln()
+    println("You said: $user_input")
+
+//task 3
+    print("Enter your fav colour ")
+    val fav_colour = readln()
+    print("Enter the lucky number ")
+    val lucky_number = readln()
+    println("Colour: $fav_colour; lucky number: $lucky_number")
+
+//task4
+    print("Name ")
+    val name = readln()
+    print("Last name ")
+    val last_name = readln()
+    print("City ")
+    val city = readln()
+    println(" Name: $name $last_name\n city: $city")
+
+//task5
+    println("She said: \"Keep coding\"")
+    println("C:\\Kotlin\\Lab01")
+//task6
+    val a = 6
+    val b = 4
+    println("$a + $b = ${a + b}")
+}
+
+
